4501b714c712f807166d807664dc555643826e96
tobelthenewone <tobelmikiyas@gmail.com>
2026-08-06 19:35:30 +0300

    add WSGI configuration for core project and API token management URLs

---
+++ backend/core/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # API Token Management
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # Project URLs
+    path('admin/', admin.site.urls),
+    path('api/user/', include('users.urls', namespace='users')),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
